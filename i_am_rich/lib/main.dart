5dc32b95a67e8db9e2cb36a49e884f8db66e2957
Andri Sedania <andri@sedaniainnovator.com>
2020-01-12 19:12:55 +0700

    Center widget

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('Hello World!')));
+void main() => runApp(MaterialApp(
+        home: Center(
+      child: Text('Hello World!'),
+    )));
