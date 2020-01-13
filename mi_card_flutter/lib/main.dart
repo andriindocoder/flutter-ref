fe06b56ca65866fb4ae8f425d6f43b52d0183ef3
Andri Sedania <andri@sedaniainnovator.com>
2020-01-13 19:34:15 +0700

    Base Stateless Widget

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Demo'),
+        ),
+        backgroundColor: Colors.white,
         body: Container(),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: Icon(Icons.add),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
