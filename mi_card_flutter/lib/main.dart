1b55a7acbf4e4bf7c8a2b53b973fe3f085829f24
Andri Sedania <andri@sedaniainnovator.com>
2020-03-03 08:49:51 +0700

    6.45 - How to use column and row widget for layout

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,13 +11,32 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 15.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.black,
+                child: Text('Container 1'),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.yellow,
+                child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
+              ),
+            ],
           ),
         ),
       ),
