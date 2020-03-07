f1b2046dd2e19d96d840ef0dfcd074d1ccccdfea
Andri Sedania <andri@sedaniainnovator.com>
2020-03-07 10:02:21 +0700

    6.46 Incorporating custom fonts in your flutter app

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -20,8 +20,19 @@ class MyApp extends StatelessWidget {
               Text(
                 'Andri Wicaksono',
                 style: TextStyle(
+                  fontFamily: 'Rubik',
                   fontSize: 40.0,
                   color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'WEB AND MOBILE APP DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.grey[700],
+                  letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               ),
