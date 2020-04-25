846f415f7e314973d38a10bf947b0ea63ce3a9dc
Andri Sedania <andri@sedaniainnovator.com>
2020-04-25 15:48:50 +0700

    Section 7.64 - Challenge 1 Solution

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -23,6 +23,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -42,10 +43,10 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
+                  rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
